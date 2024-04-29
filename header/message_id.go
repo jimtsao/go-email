7f8c54e6b14c19f0e1690b75cbbf16ebba40f521
Jim Tsao <jimtsao@users.noreply.github.com>
2024-04-30 08:42:00 +1000

    Add MessageID header

---
+++ header/message_id.go
@@ -0,0 +1,77 @@
+package header
+
+// Date represents the 'Date' header field
+//
+// Syntax:
+//
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/jimtsao/go-email/syntax"
+)
+
+// MessageID represents the 'Message-ID' header field.
+//
+// Automatically inserts @ and wraps in angle brackets if required
+//
+// Usage:
+//
+//	m := MessageID("<2024.12.31@localhost>")
+//	m := MessageID("unique") // becomes <unique@>
+//
+// Syntax:
+//
+//	message-id      =   "Message-ID:" msg-id CRLF
+//	msg-id          =   [CFWS] "<" id-left "@" id-right ">" [CFWS]
+//	id-left         =   dot-atom-text
+//	id-right        =   dot-atom-text / no-fold-literal
+//	no-fold-literal =   "[" *dtext "]"
+//	dot-atom-text   =   1*atext *("." 1*atext)
+//	atext           =   ALPHA / DIGIT /
+//						"!" / "#" / "$" / "%" / "&" / "'" / "*" /
+//						"+" / "-" / "/" / "=" / "?" / "^" / "_" /
+//						"`" / "{" /	"|" / "}" / "~"
+//	dtext           =   %d33-90 / %d94-126
+//
+// dtext: printable ascii excluding "[", "]", or "\"
+type MessageID string
+
+func (m MessageID) Name() string {
+	return "Message-ID"
+}
+
+func (m MessageID) Validate() error {
+	// folding not permitted in message-id field
+	// max line length = 998
+	maxLineLen := 998
+	maxContentLen := maxLineLen - len("Message-ID: ")
+	id := m.normalise()
+	if len(id) > maxContentLen {
+		return fmt.Errorf("message-id must not exceed %d octets, has %d octets", maxContentLen, len(id))
+	}
+
+	if !syntax.IsMessageID(id) {
+		return errors.New("message-id invalid syntax")
+	}
+
+	return nil
+}
+
+func (m MessageID) String() string {
+	return fmt.Sprintf("Message-ID: %s\r\n", m.normalise())
+}
+
+// normalise trims whitespace and inserts @ and < > if needed
+func (m MessageID) normalise() string {
+	s := string(m)
+	s = strings.TrimSpace(s)
+	if !strings.Contains(s, "@") {
+		s = s + "@"
+	}
+	if !strings.Contains(s, "<") && !strings.Contains(s, ">") {
+		return "<" + s + ">"
+	}
+	return s
+}
---
+++ header/message_id_test.go
@@ -0,0 +1,45 @@
+package header_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/jimtsao/go-email/header"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMessageID(t *testing.T) {
+	var longStr strings.Builder
+	maxContentLen := 998 - len("Message-ID: <@>")
+	for i := 0; i < maxContentLen; i++ {
+		longStr.WriteString("-")
+	}
+	ls := longStr.String()
+
+	type testcase struct {
+		input string
+		want  string
+		pass  bool
+	}
+
+	for _, c := range []testcase{
+		{"<message@id>", "Message-ID: <message@id>\r\n", true},
+		{"message-id", "Message-ID: <message-id@>\r\n", true},
+		{"message@id", "Message-ID: <message@id>\r\n", true},
+		{ls, "Message-ID: " + fmt.Sprintf("<%s@>", ls) + "\r\n", true},
+		{ls + "-", "Message-ID: " + fmt.Sprintf("<%s-@>", ls) + "\r\n", false},
+		{"<in:valid@char>", "Message-ID: <in:valid@char>\r\n", false},
+	} {
+		{
+			m := header.MessageID(c.input)
+			err := m.Validate()
+			if c.pass {
+				assert.NoError(t, err, c.input)
+			} else {
+				assert.Error(t, err, c.input)
+			}
+			assert.Equal(t, c.want, m.String(), c.input)
+		}
+	}
+}
