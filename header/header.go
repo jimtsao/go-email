cadb4fe5a6bba2a051de53abf122fbf75ee030b7
Jim Tsao <jimtsao@users.noreply.github.com>
2024-06-13 12:31:51 +1000

    Move header interface into own package

--- header/header.go
+++ header/header.go
@@ -1,13 +1,14 @@
-package email
+package header
 
-// Header represents an entity header field
+// Header field
 type Header interface {
 	// Name of header field, MUST be in canonical form
 	// eg. 'Reply-To' rather than 'reply-to'
 	Name() string
-	// Validate against RFC 5322 syntax
+	// Validate against relevant RFCs
 	Validate() error
-	// String outputs RFC 5322 compliant format:
+	// String outputs in format complaint with relevant RFCs.
+	// The general format is:
 	//
 	//	Header fields are lines beginning with a field name, followed by a
 	//	colon (":"), followed by a field body, and terminated by CRLF
